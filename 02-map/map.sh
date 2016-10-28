6cb375ccd0d4d3f4c1a5adfe5c82b8be16127e55
terrycojones <terry@jon.es>
2016-10-28 10:21:09 +0100

    Fixed 02-map sbatch and map.sh

--- 02-map/map.sh
+++ 02-map/map.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 task=$1
 fastq=../01-trim/$task-trimmed.fastq.gz
@@ -6,10 +6,10 @@ fastq=../01-trim/$task-trimmed.fastq.gz
 . /home/tcj25/.virtualenvs/35/bin/activate
 
 # Map to human genome, save to ramdisk SAM file.
-bwa mem -t 24 $HOME/scratch/homo-sapiens/homo-sapiens $fastq > /ramdisks/terry-$basename.sam
+bwa mem -t 24 $HOME/scratch/homo-sapiens/homo-sapiens $fastq > /ramdisks/terry-$task.sam
 
 # Convert SAM to BAM, on ramdisk.
-samtools view -bS /ramdisks/terry-$basename.sam > /ramdisks/terry-$basename.bam
+samtools view --threads 24 -bS /ramdisks/terry-$task.sam > /ramdisks/terry-$task.bam
 
 # Sort ramdisk BAM, writing to persisted local storage.
-samtools sort --threads 24 -o $basename.bam /ramdisks/terry-$basename.bam
+samtools sort --threads 24 -o $task.bam /ramdisks/terry-$task.bam
--- 02-map/sbatch.sh
+++ 02-map/sbatch.sh
@@ -5,5 +5,7 @@ task=$1
 echo "task is $task" > sbatch-$task.out
 echo "deps are $SP_DEPENDENCY_ARG" >> sbatch-$task.out
 
-jobid=`sbatch -n 1 $SP_DEPENDENCY_ARG submit.sh $task | cut -f4 -d' '`
+# Request an exclusive machine because we're going to tell bwa and samtools
+# to use 24 threads.
+jobid=`sbatch -n 1 --exclusive $SP_DEPENDENCY_ARG submit.sh $task | cut -f4 -d' '`
 echo "TASK: $task $jobid"
