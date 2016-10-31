c40bdc985335227d96619be4006e436a679dfaa5
terrycojones <terry@jon.es>
2016-10-31 17:02:47 +0000

    Added SBATCH directives to stop.sh

--- 06-stop/stop.sh
+++ 06-stop/stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+#SBATCH -J stop
+#SBATCH -A DSMITH-BIOCLOUD
+#SBATCH -o slurm-%A.out
+#SBATCH -p biocloud-normal
+#SBATCH --time=5:00:00
+
 . /home/tcj25/.virtualenvs/35/bin/activate
 
 log=../slurm-pipeline.log
